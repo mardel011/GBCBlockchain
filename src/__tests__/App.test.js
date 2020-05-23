2b5b529dc23bfba213065260a43f5dc74e501cb5
mardel011 <59632640+mardel011@users.noreply.github.com>
2020-05-23 15:19:43 -0400

    Updated file

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App.css'
+import ReactDOM from 'react-dom';
+import App from './App';
 
-//test('renders learn react link', () => {
- //const { getByText } = render(<App />);
- // const linkElement = getByText(/learn react/i);
- // expect(linkElement).toBeInTheDocument();
-//});
-
-afterEach(cleanup)
-test('It has to contain header2', () => {
-  render(
-    <div>
-      <App />
-    </div>
-  )
-  const headerClass = header2().type.styledComponentId
-  const MyHeaderRoots = document.getElementsByClassName(headerClass)
-  const style = window.getComputedStyle(MyHeaderRoots[0])
-  expect(style.margin-bottom).toBe('100px')
-})
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
