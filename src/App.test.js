15277b753ae59c0e28dd0c912de62d0b4042c53c
mardel011 <59632640+mardel011@users.noreply.github.com>
2020-05-23 15:44:35 -0400

    Updated app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CSS from './App.css'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+afterEach(cleanup)
+test('It has to contain header2', () => {
+  render(
+    <div>
+      <CSS />
+    </div>
+  )
+  const headerClass = header2().type.styledComponentId
+  const MyHeaderRoots = document.getElementsByClassName(headerClass)
+  const style = window.getComputedStyle(MyHeaderRoots[0])
+  expect(style.margin-bottom).toBe('100px')
 });
