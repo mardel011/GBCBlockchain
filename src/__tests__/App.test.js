d12a413aae6fd85f7ab2b9defeaaa088a929b6d0
mardel011 <59632640+mardel011@users.noreply.github.com>
2020-05-23 15:17:08 -0400

    Updated file

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App.css'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+//test('renders learn react link', () => {
+ //const { getByText } = render(<App />);
+ // const linkElement = getByText(/learn react/i);
+ // expect(linkElement).toBeInTheDocument();
+//});
 
 afterEach(cleanup)
 test('It has to contain header2', () => {
